11fe86044e63a2f87d654325dc4eae1e9ac4ac45
Harnish Savadia <66225151+harnish7576@users.noreply.github.com>
2024-12-28 15:56:39 -0500

    Add files via upload

--- Lambda/registration-form.py
+++ Lambda/registration-form.py
@@ -1,1 +1,26 @@
+import json
+import boto3
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('form-table')
+
+def lambda_handler(event, context):
+    print(event)
+
+    response = table.put_item(
+        Item={
+            'email': event['email'],
+            'name': event['name'],
+            'phone': event['phone'],
+            'password': event['password']
+        }
+    )
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': json.dumps({'message': 'Registration successful'})
+    }
